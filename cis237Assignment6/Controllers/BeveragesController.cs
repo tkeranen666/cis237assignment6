299c4ff315081fa8f918eb4454cd6174df74b9cb
Tim Keranen <tkeranen@mail.kvcc.edu>
2016-04-24 13:48:29 -0700

    Add Beverages Model. Add link to Beverage list at the top of the web page.

---
+++ cis237Assignment6/Controllers/BeveragesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cis237Assignment6;
+
+namespace cis237Assignment6.Controllers
+{
+    [Authorize]
+    public class BeveragesController : Controller
+    {
+        private BeverageEntities db = new BeverageEntities();
+
+        // GET: Beverages
+        public ActionResult Index()
+        {
+            return View(db.Beverages.ToList());
+        }
+
+        // GET: Beverages/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beverage beverage = db.Beverages.Find(id);
+            if (beverage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Beverages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name,pack,price,active")] Beverage beverage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Beverages.Add(beverage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(beverage);
+        }
+
+        // GET: Beverages/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beverage beverage = db.Beverages.Find(id);
+            if (beverage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beverage);
+        }
+
+        // POST: Beverages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,pack,price,active")] Beverage beverage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(beverage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Beverage beverage = db.Beverages.Find(id);
+            if (beverage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(beverage);
+        }
+
+        // POST: Beverages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Beverage beverage = db.Beverages.Find(id);
+            db.Beverages.Remove(beverage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
